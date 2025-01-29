d518b967fdee35ba2ca5cb87fe7ab5660fc1e09d
roshaanrashidkhan <roshaan.rashid.r50@gmail.com>
2025-01-29 13:33:30 +0500

    waiting session updated

--- resources/views/dashboard_patient/Waiting_room/waiting.blade.php
+++ resources/views/dashboard_patient/Waiting_room/waiting.blade.php
@@ -13,6 +13,7 @@
     <script src="{{ asset('/js/app.js') }}"></script>
     <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @php
     header("Access-Control-Allow-Origin: *");
     @endphp
@@ -54,9 +55,9 @@
                     <h3 class="sess-title">Session with Dr.<b>{{ $session->doctor_name }} </b> Ended</h3>
                     <div>
                         <input type="hidden" id="session_id" value="{{ $session->id }}">
-                        <img src="{{ asset('assets/new_frontend/logo.png') }}" alt="" style="width:10% ; margin:20px 0;">
+                        <img src="{{ asset('assets/new_frontend/logo.png') }}" alt="" style="width:10% ; margin:10px 0;">
                     </div>
-                    <div class="mt-2">
+                    <div class="mt-1">
                         <h3>Please Wait!</h3>
                         <h4>Doctor is finalizing your prescription and adding services to your cart</h4>
                         <h6>( Estimated time <b>5 Minutes</b> )</h6>
@@ -68,6 +69,20 @@
                     </div>
                     <div>
                         <p><b>Attention : </b><span class="text-danger">Please don't refresh this page </span> </p>
+                    </div>
+                    <div class="or-container my-3">
+                        <span class="or-design">OR</span>
+                    </div>
+                    <div class="mt-1 mx-auto text-center">
+                        <p class="text-danger">If you encounter any issues during your session, contact us:</p>
+                        <div class="d-flex align-items-center justify-content-center">
+                            <i class="fa-solid fa-envelope"></i>
+                            <a href="mailto:contact@communityhealthcareclinics.com" target="_blank">contact@communityhealthcareclinics.com</a>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-center">
+                            <i class="fa-brands fa-whatsapp text-success"></i>
+                            <a href="https://wa.me/923372350684" target="_blank">0337-2350684</a>
+                        </div>
                     </div>
                 </div>
             </div>
