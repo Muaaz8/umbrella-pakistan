e8281b105869f47528ed2263ef55afa5c72ca137
roshaanrashidkhan <roshaan.rashid.r50@gmail.com>
2025-02-03 18:26:49 +0500

    contact page and styles updated

--- resources/views/website_pages/new_pakistan_contact_us.blade.php
+++ resources/views/website_pages/new_pakistan_contact_us.blade.php
@@ -95,7 +95,6 @@
                         <a href="https://www.facebook.com/CommunityHealthcareClinics"><i class="fa-brands fa-facebook"></i></a>
                         <a href="#"><i class="fa-brands fa-linkedin"></i></a>
                         <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                        <a href="#"><i class="fa-brands fa-pinterest"></i></a>
                     </div>
 
 
