257988f09c1773526f6316653a707b93dac12d62
Muaaz8 <111640774+Muaaz8@users.noreply.github.com>
2025-02-13 17:02:54 +0500

    login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Events\loadOnlineDoctor;
 use Illuminate\Validation\ValidationException;
-use App\Models\User;
+use App\User;
 
 class LoginController extends Controller
 {
