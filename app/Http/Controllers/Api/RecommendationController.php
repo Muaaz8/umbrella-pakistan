32b880a07f53b66f6926487f1e67ccc63df6d651
zaid617 <zaid6177@gmail.com>
2025-06-17 17:53:15 +0500

    recommendation fix

--- app/Http/Controllers/Api/RecommendationController.php
+++ app/Http/Controllers/Api/RecommendationController.php
@@ -44,40 +44,10 @@ class RecommendationController extends BaseController
         Session::where('id', $session_id)->update(['validation_status' => 'expired']);
         if ($items > 0) {
             foreach ($pres_list as $pres) {
-                $product = DB::table('tbl_products')->where('id', $pres->medicine_id)->first();
                 if ($pres->type == "medicine") {
-                    $med_unit = DB::table('medicine_units')->where('unit',$pres->med_unit)->first();
-                    $price = DB::table('medicine_pricings')
-                        ->where('product_id', $pres->medicine_id)
-                        ->where('unit_id',$med_unit->id)
-                        ->first();
-                    $prescription = DB::table('prescriptions')->where('id',$pres->id)->first();
-                    if($product->is_single){
-                        $pres->price = ($prescription->med_time*$prescription->med_days)*$price->sale_price;
-                    }else{
-                        $pres->price = $price->sale_price;
-                    }
-                    $up = DB::table('prescriptions')->where('id',$pres->id)->update(['price' => $price->id]);
-                    Cart::create([
-                        'product_id' => $pres->medicine_id,
-                        'name' => $product->name,
-                        'quantity' => $pres->quantity,
-                        'price' => $pres->price,
-                        'update_price' => $pres->price * $pres->quantity,
-                        'product_mode' => $pres->type,
-                        'user_id' => $session->patient_id,
-                        'doc_id' => $session->doctor_id,
-                        'doc_session_id' => $session_id,
-                        'pres_id' => $pres->id,
-                        'item_type' => 'prescribed',
-                        'status' => 'recommended',
-                        'checkout_status' => 1,
-                        'purchase_status' => 1,
-                        'product_image' => $product->featured_image
-                    ]);
+                    $product = DB::table('tbl_products')->where('id', $pres->medicine_id)->first();
                     $singleItemMedicine = [
                         'medicine_name' => $product->name,
-                        'unit' => $pres->med_unit,
                         'days' => $pres->med_days,
                         'quantity' => $pres->quantity,
                         'usage' => $pres->usage,
@@ -85,51 +55,17 @@ class RecommendationController extends BaseController
                     ];
                     array_push($prePharma, $singleItemMedicine);
                 } else if ($pres->type == "lab-test") {
-                    $lab_test_price = QuestDataTestCode::where('TEST_CD', $pres->test_id)->first();
-                    Cart::create([
-                        'product_id' => $pres->test_id,
-                        'name' => $lab_test_price->TEST_NAME,
-                        'quantity' => $pres->quantity,
-                        'price' => $lab_test_price->SALE_PRICE,
-                        'update_price' => $lab_test_price->SALE_PRICE * $pres->quantity,
-                        'product_mode' => $pres->type,
-                        'user_id' => $session->patient_id,
-                        'doc_id' => $session->doctor_id,
-                        'doc_session_id' => $session_id,
-                        'pres_id' => $pres->id,
-                        'item_type' => 'prescribed',
-                        'status' => 'recommended',
-                        'checkout_status' => 1,
-                        'purchase_status' => 1,
-                        'product_image' => $lab_test_price->featured_image,
-                    ]);
+                    $product = DB::table('quest_data_test_codes')->where('TEST_CD', $pres->test_id)->first();
                     $singleItemTest = [
-                        'test_name' => $lab_test_price->TEST_NAME,
+                        'test_name' => $product->TEST_NAME,
                         'quantity' => $pres->quantity,
                         'comment' => $pres->comment,
                     ];
                     array_push($preLab, $singleItemTest);
                 } else if ($pres->type == "imaging") {
-                    $lab_test_price = QuestDataTestCode::where('TEST_CD', $pres->test_id)->first();
-                    Cart::create([
-                        'product_id' => $pres->test_id,
-                        'name' => $lab_test_price->TEST_NAME,
-                        'quantity' => $pres->quantity,
-                        'price' => $lab_test_price->SALE_PRICE,
-                        'update_price' => $lab_test_price->SALE_PRICE * $pres->quantity,
-                        'product_mode' => $pres->type,
-                        'user_id' => $session->patient_id,
-                        'doc_id' => $session->doctor_id,
-                        'doc_session_id' => $session_id,
-                        'pres_id' => $pres->id,
-                        'item_type' => 'prescribed',
-                        'status' => 'recommended',
-                        'checkout_status' => 1,
-                        'purchase_status' => 1,
-                        'product_image' => $lab_test_price->featured_image,
-                    ]);
+                    $product = DB::table('quest_data_test_codes')->where('TEST_CD', $pres->test_id)->first();
                     $singleItemTest = [
-                        'test_name' => $lab_test_price->TEST_NAME,
+                        'test_name' => $product->TEST_NAME,
                         'quantity' => $pres->quantity,
                         'comment' => $pres->comment,
                     ];
@@ -149,49 +85,44 @@ class RecommendationController extends BaseController
         }
 
         // try {
-            array_push($dataMarge, array('patient' => $patient_user));
-            array_push($dataMarge, array('doctor' => $doctor_user));
-            array_push($dataMarge, array('rec_test' => $preLab));
-            array_push($dataMarge, array('rec_pharma' => $prePharma));
-            array_push($dataMarge, array('rec_imaging' => $preImaging));
-            array_push($dataMarge, array('pat_email' => ucwords($patient_user->email)));
-            array_push($dataMarge, array('session' => $session));
-            $pdf = PDF::loadView('onlineprescriptionPdf',compact('dataMarge'));
-            try{
-                Mail::send('emails.prescriptionEmail', ['user_data'=>$patient_user], function ($message) use ($patient_user,$dataMarge,$pdf) {
-                    $message->to($patient_user->email)->subject('patient prescription')->attachData($pdf->output(), "prescription.pdf");
-                });
-            }catch(\Exception $e){
+        array_push($dataMarge, array('patient' => $patient_user));
+        array_push($dataMarge, array('doctor' => $doctor_user));
+        array_push($dataMarge, array('rec_test' => $preLab));
+        array_push($dataMarge, array('rec_pharma' => $prePharma));
+        array_push($dataMarge, array('rec_imaging' => $preImaging));
+        array_push($dataMarge, array('pat_email' => ucwords($patient_user->email)));
+        array_push($dataMarge, array('session' => $session));
+        $pdf = PDF::loadView('onlineprescriptionPdf', compact('dataMarge'));
+        Mail::send('emails.prescriptionEmail', ['user_data' => $patient_user], function ($message) use ($patient_user, $dataMarge, $pdf) {
+            $message->to($patient_user->email)->subject('patient prescription')->attachData($pdf->output(), "prescription.pdf");
+        });
 
-            }
+        $pdfData = $pdf->output();
 
-            $pdfData = $pdf->output();
+        $tempFile = tmpfile();
+        fwrite($tempFile, $pdfData);
+        $metaData = stream_get_meta_data($tempFile);
+        $filePath = $metaData['uri'];
 
-            $tempFile = tmpfile();
-            fwrite($tempFile, $pdfData);
-            $metaData = stream_get_meta_data($tempFile);
-            $filePath = $metaData['uri'];
+        UploadMediaJob::dispatch($filePath, $patient_user);
 
-            UploadMediaJob::dispatch($filePath,$patient_user);
-
-            $text = "Session Complete Please Check Recommendations";
-            $notification_id = Notification::create([
-                'user_id' => $patient_user->id,
-                'type' => '/my/cart',
-                'text' => $text,
-                'session_id' => $session_id,
-            ]);
-            $data = [
-                'user_id' => $patient_user->id,
-                'type' => '/my/cart',
-                'text' => $text,
-                'session_id' => $session_id,
-                'received' => 'false',
-                'appoint_id' => 'null',
-                'refill_id' => 'null',
-            ];
-            event(new RealTimeMessage($patient_user->id));
-
+        $text = "Session Complete Please Check Recommendations";
+        $notification_id = Notification::create([
+            'user_id' => $patient_user->id,
+            'type' => '/my/cart',
+            'text' => $text,
+            'session_id' => $session_id,
+        ]);
+        $data = [
+            'user_id' => $patient_user->id,
+            'type' => '/my/cart',
+            'text' => $text,
+            'session_id' => $session_id,
+            'received' => 'false',
+            'appoint_id' => 'null',
+            'refill_id' => 'null',
+        ];
+        event(new RealTimeMessage($patient_user->id));
         if ($items > 0) {
             ActivityLog::create([
                 'activity' => 'Create session recommendations for ' . $patient_user->name . " " . $patient_user->last_name,
@@ -230,6 +161,7 @@ class RecommendationController extends BaseController
                 'pat_email' => $pat_detail->email,
                 'comment' => $referal->comment,
             ];
+            // dd($data,$data1);
 
             try {
                 Mail::to($pat_detail->email)->send(new ReferDoctorToPatientMail($data));
@@ -243,18 +175,18 @@ class RecommendationController extends BaseController
                     'status' => 'new',
                     'session_id' => $session_id,
                 ]);
-                $text = 'Dr.'.$refer_doc_detail->name.' '.$refer_doc_detail->last_name.' reffered (Click & book an appointment)';
+                $text = 'Dr.' . $refer_doc_detail->name . ' ' . $refer_doc_detail->last_name . ' reffered (Click & book an appointment)';
                 $notification_id2 = Notification::create([
                     'user_id' => $referal->patient_id,
                     'text' => $text,
-                    'type' => '/view/doctor/'.\Crypt::encrypt($refer_doc_detail->id),
+                    'type' => '/view/doctor/' . \Crypt::encrypt($refer_doc_detail->id),
                     'status' => 'new',
                     'session_id' => $session_id,
                 ]);
                 $data = [
                     'user_id' => $referal->patient_id,
                     'text' => $text,
-                    'type' => '/view/doctor/'.\Crypt::encrypt($refer_doc_detail->id),
+                    'type' => '/view/doctor/' . \Crypt::encrypt($refer_doc_detail->id),
                     'session_id' => $session_id,
                     'received' => 'false',
                     'appoint_id' => 'null',
@@ -275,12 +207,12 @@ class RecommendationController extends BaseController
                 $sessionData = Session::find($session_id);
                 $sessionData->received = false;
                 event(new redirectToCart($session->id));
-                return $this->sendResponse(['action'=>'OnlineWaitingRoom'], 'Recommendation Created Successfully');
+                return $this->sendResponse(['action' => 'OnlineWaitingRoom'], 'Recommendation Created Successfully');
             }
         }
         $sessionData = Session::find($session_id);
         $sessionData->received = false;
         event(new redirectToCart($session->id));
-        return $this->sendResponse(['action'=>'OnlineWaitingRoom'], 'Recommendation Created Successfully');
+        return $this->sendResponse(['action' => 'OnlineWaitingRoom'], 'Recommendation Created Successfully');
     }
 }
